7639b52ef299d1c114eaddd67c901a4f7a96f401
Johnpaul627 <bellojohnpaul25@gmail.com>
2024-09-20 15:43:44 +0800

    first commit

---
+++ Activity.java
@@ -0,0 +1,23 @@
+class activity{
+    public static void main(string [] args ) {
+
+        Dog puppy = new Dog ();
+
+        puppy.setName(name:"aso");
+
+        system.out.println(puppy.showName());
+
+        puppy.bark();
+
+        puppy.listofpuppies = new string[] {"nongni", "ninong", "paul", "bello", "loyd", "dyol"};
+
+        int shownumber = puppy.show_numofpuppies();
+
+        system.out.println("Number of puppies: " + shownumber);
+
+        puppy.show_numofpuppies();
+
+        puppy.showpuppies();
+
+    }
+}
